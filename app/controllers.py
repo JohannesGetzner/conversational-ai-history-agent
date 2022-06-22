5c5a2daef9f53ee84e30b64058da6e1f2fd879bf
18817 <axingzxx@gmail.com>
2022-06-22 12:21:23 +0200

    com

--- app/controllers.py
+++ app/controllers.py
@@ -46,15 +46,16 @@ def get_person_by_location(city, country, continent):
     df = df[match]
     if df.empty:
         return None
-    person = df.head(1)
-
+    person = df.head(10)
+    person = person.sample()
     return person
 
 
 def get_id_by_occu(occu):
     df = read_dataset()
     df = df.loc[df['occupation'] == occu]
-    person = df.head(1)
+    person = df.head(10)
+    person = person.sample()
     name = person.full_name.item()
     occupation = person.occupation.item()
     person_id = person.person_id.item()
