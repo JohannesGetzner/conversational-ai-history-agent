918bbff8ab67dfd51a95a5dea66eb7b5d469b764
JohannesGetzner <ge65fin@mytum.de>
2022-07-01 00:05:37 +0200

    added agent to flask "page"

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@
 @author: sebis
 """
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_dialogflow.agent import DialogflowAgent
 
 # create app and agent instances
@@ -14,7 +14,7 @@ agent = DialogflowAgent(app=app, route="/", templates_file="templates/templates.
 # set up test route
 @app.route("/")
 def hello_world():
-    return "<p>Hello world.</p>"
+    return render_template('frontend.html')
 
 # import main conversation handlers for webhooks
 from app import webhooks
