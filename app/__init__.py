22aea6582cb978a9c0bf66679c57fa1cd47ea123
JohannesGetzner <ge65fin@mytum.de>
2022-11-04 18:32:00 +0100

    refactored init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-
-@author: sebis
-"""
-
 from flask import Flask, render_template
 from flask_dialogflow.agent import DialogflowAgent
+from app import webhooks
 
-# create app and agent instances
 app = Flask(__name__)
 agent = DialogflowAgent(app=app, route="/", templates_file="templates/templates.yaml")
 
-# set up test route
-@app.route("/")
-def hello_world():
-    return render_template('frontend.html')
 
-# import main conversation handlers for webhooks
-from app import webhooks
+@app.route("/")
+def agent_template():
+    return render_template('frontend.html')
